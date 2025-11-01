ac8fffa1be69a912a3b2d6f133ba51e031616d0b
William minerva <willia2minerva@gmail.com>
2025-11-01 15:22:15 +0100

    Create package.js

---
+++ package.js
@@ -0,0 +1,19 @@
+{
+  "name": "bankgold-bot",
+  "version": "1.0.0",
+  "description": "رفيقك في عالم عملات الغولد",
+  "main": "server.js",
+  "scripts": {
+    "start": "node server.js",
+    "dev": "nodemon server.js"
+  },
+  "dependencies": {
+    "express": "^4.18.2",
+    "axios": "^1.5.0",
+    "sqlite3": "^5.1.6",
+    "dotenv": "^16.3.1"
+  },
+  "devDependencies": {
+    "nodemon": "^3.0.1"
+  }
+}
