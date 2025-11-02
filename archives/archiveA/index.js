06c029eb7718b6d3b888192096218f9a4214df34
William minerva <willia2minerva@gmail.com>
2025-11-02 11:13:11 +0100

    Create index.js

---
+++ archives/archiveA/index.js
@@ -0,0 +1,25 @@
+const A1 = require('./A1');
+const A2 = require('./A2');
+const A3 = require('./A3');
+const A4 = require('./A4');
+const A5 = require('./A5');
+const A6 = require('./A6');
+const A7 = require('./A7');
+const A8 = require('./A8');
+const A9 = require('./A9');
+const A10 = require('./A10');
+
+const archiveA = {
+  A1,
+  A2,
+  A3,
+  A4,
+  A5,
+  A6,
+  A7,
+  A8,
+  A9,
+  A10
+};
+
+module.exports = archiveA;
