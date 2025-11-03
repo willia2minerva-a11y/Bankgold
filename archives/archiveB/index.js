2a97b9985c91dd4e4e3ddfa4e3b92c463db567b8
William minerva <willia2minerva@gmail.com>
2025-11-03 11:46:09 +0100

    Create index.js

---
+++ archives/archiveB/index.js
@@ -0,0 +1,21 @@
+const B1 = require('./B1');
+const B2 = require('./B2');
+const B3 = require('./B3');
+const B4 = require('./B4');
+const B5 = require('./B5');
+const B6 = require('./B6');
+const B7 = require('./B7');
+const B8 = require('./B8');
+
+const archiveB = {
+  B1,
+  B2,
+  B3,
+  B4,
+  B5,
+  B6,
+  B7,
+  B8
+};
+
+module.exports = archiveB;
