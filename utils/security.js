be2fa40c957e8591e2292dbfdb6c2731e2725a63
William minerva <willia2minerva@gmail.com>
2025-11-11 09:32:43 +0100

    Update security.js

--- utils/security.js
+++ utils/security.js
@@ -13,9 +13,24 @@ function generateUserCode() {
     let currentLetter = config.currentLetter;
     let currentNumber = config.currentNumber + 1;
     
-    if (currentNumber > 999) {
-        currentNumber = 1;
+    // إذا وصلنا إلى B999، ننتقل إلى C001
+    if (currentLetter === 'B' && currentNumber > 999) {
         currentLetter = 'C';
+        currentNumber = 1;
+    }
+    // إذا وصلنا إلى C999، ننتقل إلى D001 وهكذا...
+    else if (currentLetter === 'C' && currentNumber > 999) {
+        currentLetter = 'D';
+        currentNumber = 1;
+    }
+    else if (currentLetter > 'B' && currentNumber > 999) {
+        currentLetter = String.fromCharCode(currentLetter.charCodeAt(0) + 1);
+        currentNumber = 1;
+    }
+    // إذا تجاوزنا Z، نعود إلى A (نظرياً)
+    else if (currentLetter > 'Z') {
+        currentLetter = 'A';
+        currentNumber = 1;
     }
     
     // حفظ الإعدادات الجديدة
